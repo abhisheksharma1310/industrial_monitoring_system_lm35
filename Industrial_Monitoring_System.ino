5bd8440fd5cceb81408b656f5a1799e4aa43d2aa
ABHISHEK SHARMA <43085397+abhisheksharma1310@users.noreply.github.com>
2021-06-25 15:39:43 +0530

    Update Industrial_Monitoring_System.ino

--- Industrial_Monitoring_System.ino
+++ Industrial_Monitoring_System.ino
@@ -11,6 +11,7 @@ const char* myWriteAPIKey = "";  //ThingSpeak API Key
 WiFiClient client; //make object client of class WiFiClient
 #define sensor A0  //Pin Connected to output of LM35 
 int val; //variable for storing sensor value
+
 //******************************************* Setup Part ************************************************
 void setup() 
 {
@@ -31,13 +32,14 @@ void setup()
   //connect to ThingSpeak
   ThingSpeak.begin(client);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(sensor);  //analog data stored in val variable
   float mv = ( val/1024.0)*1000; //converted into mili volt
   float cel = mv/10; //1 cel temperature equal to 10mv 
-  float farh = (cel*9)/5 + 32; //convert celcius into fahrenheit
-  //Print temperature in degree celcius on Serial monitor
+  float farh = (cel*9)/5 + 32; //convert celsius into fahrenheit
+  //Print temperature in degree celsius on Serial monitor
   Serial.print("TEMPRATURE = ");
   Serial.print(cel);
   Serial.print("*C");
@@ -47,7 +49,7 @@ void loop() {
   Serial.print(farh);
   Serial.print("*F");
   Serial.println();
-  //If temperature is grater than 25 celcius then turn on Led else turn off led
+  //If temperature is grater than 25 celsius then turn on Led else turn off led
   if(cel>25){
     digitalWrite(LED_BUILTIN,LOW);
   }
